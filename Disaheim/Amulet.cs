bfceb487f75ea999740d0cc33b240d5f8171fe37
Rasmus Nørby <rasmus.norby@gmail.com>
2023-10-06 08:33:13 +0200

    Added initial classes

---
+++ Disaheim/Amulet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    internal class Amulet
+    {
+        public string ItemID { get; set; }
+        public string Design { get; set; }
+        public Level Quality { get; set; }
+
+        public Amulet(string itemID, Level quality, string design)
+        {
+            ItemID = itemID;
+            Design = design;
+            Quality = quality;
+        }
+
+        public Amulet(string itemID, Level quality) : this(itemID, quality, "Ikke angivet")
+        {
+
+        }
+
+        public Amulet(string itemID) : this(itemID, Level.low, "Ikke angivet")
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Disaheim/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    internal class Book
+    {
+        public string ItemID { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+        public Book(string itemID, string title, double price)
+        {
+            ItemID = itemID;
+            Title = title;
+            Price = price;
+        }
+
+        public Book(string itemID, string title) : this(itemID, title, 0)
+        {
+
+        }
+
+        public Book(string itemID) : this(itemID, "Ikke angivet", 0)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
