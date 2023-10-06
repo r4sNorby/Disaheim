f1d39e949a7235da08e188ef72e05c75fac4de68
Rasmus Nørby <rasmus.norby@gmail.com>
2023-10-06 10:12:04 +0200

    Made bonus project

---
+++ BonusProjectSafe/Menu.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusProjectSafe
+{
+    public class Menu
+    {
+        string title = "";
+        List<string> options = new();
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public List<string> Options
+        {
+            get { return options; }
+            set { options = value; }
+        }
+
+        public void Show(int selectedIndex)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(title);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            foreach (string item in options)
+            {
+                if (options[selectedIndex] == item)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("> ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+
+                if (item == "Tilbage" || item == "Log ud")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(item);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
---
+++ BonusProjectSafe/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Reflection;
+
+namespace BonusProjectSafe
+{
+    internal class Program
+    {
+        static SafeState state = SafeState.locked;
+        static void Main(string[] args)
+        {
+            int index = 0;
+
+            while (true)
+            {
+                // (Re)Create menu
+                Menu menu = new();
+                
+                // Add menu-options depending on safe state
+                if (state == SafeState.locked)
+                {
+                    menu.Options.Add("Unlock");
+                }
+                else if (state == SafeState.closed)
+                {
+                    menu.Options.Add("Lock");
+                    menu.Options.Add("Open");
+                }
+                else if (state == SafeState.open)
+                {
+                    menu.Options.Add("Close");
+                }
+
+                // Create title for menu telling the state of the safe
+                menu.Title = $"The safe is {state}\n";
+
+                // Display menu and title
+                menu.Show(index);
+
+                // Listen for up/down arrows and Enter
+                ConsoleKeyInfo keyinfo = Console.ReadKey(true);
+                switch (keyinfo.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        // Move down in menu
+                        if (index + 1 < menu.Options.Count) index++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        // Move up in menu
+                        if (index - 1 >= 0) index--;
+                        break;
+                    case ConsoleKey.Enter:
+                        // Select what to do with safe
+                        SetState(menu.Options[index]);
+                        index = 0;
+                        break;
+                }
+            }
+        }
+
+        static void SetState(string chosenAction)
+        {
+            // Change state depending on what was chosen
+            switch (chosenAction)
+            {
+                case "Unlock":
+                    state = SafeState.closed;
+                    break;
+                case "Lock":
+                    state = SafeState.locked;
+                    break;
+                case "Open":
+                    state = SafeState.open;
+                    break;
+                case "Close":
+                    state = SafeState.closed;
+                    break;
+            }
+        }
+    }
+}
