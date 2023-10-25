111bb813933e6a82e110313a91b8fc48f7109361
Naywia <48431873+Naywia@users.noreply.github.com>
2023-10-25 09:16:00 +0200

    Pr16

---
+++ Disaheim/Utility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Utility
+    {
+        public double GetValueOfBook(Book book)
+        {
+            return book.Price;
+        }
+
+        public double GetValueOfAmulet(Amulet amulet)
+        {
+            switch (amulet.Quality)
+            {
+                case Level.low:
+                    return 12.5;
+                case Level.medium:
+                    return 20.0;
+                case Level.high:
+                    return 27.5;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
